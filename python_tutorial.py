7377034c9bb9407d761a20d537a132182c0053d9
Kartick Mandi <80804574+kmandi@users.noreply.github.com>
2021-03-31 16:26:37 +0530

    Create python_tutorial.py

---
+++ python_tutorial.py
@@ -0,0 +1,12 @@
+''' ...Welcome To learn python!...'''
+# Print 
+print("Hi.. I'm Kartick Mandi") # This section print simple you write under Double Quotation
+print('*' * 10) # This section print Asterisk(*) ten time  
+ 
+# Initialization & Arithmatic operation
+price = 10  # integar value
+rating = 4.9 # Floting point
+name = 'Mose' # String
+Name = "Tiger" # # You can also initialize string under Double Quotation
+is_published = True # boolean value
+print(price)
